687cfbd2622c61c88abd228e08312d46611038ee
CharissaYeong <donutdandy@gmail.com>
2023-05-08 06:58:07 +0000

    added prod hbs

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,11 @@ const router = express.Router(); // #1 - Create a new express Router
 
 //  #2 Add a new route to the Express router
 router.get('/', (req,res)=>{
-    res.send("All products")
+    res.render('products/products')
 })
 
 router.get('/create', (req,res)=>{
-    res.send("Create a new product")
+    res.render('products/create')
 })
 
 module.exports = router; // #3 export out the router
