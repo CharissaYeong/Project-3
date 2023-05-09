ad40d75c69ab88f6599b66ebbdb7c5e4fe45728e
CharissaYeong <donutdandy@gmail.com>
2023-05-09 16:44:21 +0000

    migrate product

--- migrations/20230508135331-products.js
+++ migrations/20230508135331-products.js
@@ -14,14 +14,28 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
+// exports.up = function(db) {
+//   return db.createTable('product', {
+//       id: { type: 'int', primaryKey:true, autoIncrement:true, unsigned: true},
+//       name: { type: 'string', length:100, notNull:true },
+//       base_price: { type: 'int', unsigned: true, notNull:true }, 
+//       description:'text',
+//       stock: { type: 'int', unsigned: true, notNull:true }
+//   })
+// };
+
 exports.up = function(db) {
   return db.createTable('product', {
-      id: { type: 'int', primaryKey:true, autoIncrement:true, unsigned: true},
-      name: { type: 'string', length:100, notNull:true },
-      base_price: { type: 'int', unsigned: true, notNull:true }, 
-      description:'text',
-      stock: { type: 'int', unsigned: true, notNull:true }
-  })
+    id: { type: 'int', primaryKey:true, autoIncrement:true, unsigned: true},
+    name: { type: 'string', length:100, notNull:true },
+    description: 'text',
+    base_price: { type: 'string', unsigned: true, notNull:true },
+    option_price: 'string',
+    stock: { type: 'int', unsigned: true, notNull:true },
+    created: { type: 'timestamp', defaultValue: new String('CURRENT_TIMESTAMP') },
+    modified: { type: 'timestamp', defaultValue: new String('CURRENT_TIMESTAMP') },
+    last_edit: 'string'
+  });
 };
 
 exports.down = function(db) {
